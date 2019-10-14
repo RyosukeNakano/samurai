2b7944a32a2e02904b992038b843e3d9ca46504a
yuya.takao <takaoyuya@gmail.com>
2019-10-15 00:02:17 +0900

    [mod] git reset

--- rails/init.sql
+++
@@ -1,8 +0,0 @@
-CREATE USER 'myuser'@'%' IDENTIFIED BY 'P@ssw0rd';
-CREATE USER 'flyway'@'%' IDENTIFIED BY 'P@ssw0rd';
-
-CREATE DATABASE flywaydb;
-CREATE DATABASE mydb;
-
-GRANT ALL ON *.* TO 'myuser'@'%';
-GRANT ALL ON *.* TO 'flyway'@'%';
--- text.js
+++
@@ -1,7 +0,0 @@
-alert("Hello");
-
-var scrollTop = (function () {
-    window.scrollTo(0, 0);
-    console.log("scroll top call");
-
-}());
